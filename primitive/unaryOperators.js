0c70e94e5f48a1d9adde8a3923c6fb27aa7d8cc0
collins anaba <evulukwu@me.com>
2020-05-13 17:01:12 -0500

    updated  numberjs

---
+++ primitive/unaryOperators.js
@@ -0,0 +1,15 @@
+let score = 0
+
+
+score = score + 10
+//aka
+score += 10
+
+score -=10
+
+let bonus = 3
+
+score *= bonus
+
+//subtracts 1
+score--
---
+++ primitive/variables.js
@@ -0,0 +1,9 @@
+//variables are like labeled jars
+
+let variable = 'value'
+
+// const cannot change
+const value =  'primitive'
+
+//cant name your variable document 
+
